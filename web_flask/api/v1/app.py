72d9a7508919804a7cba82da6ca6b35c83164c1b
IhebChatti <iheb.chatti@holbertonschool.com>
2020-10-11 20:40:56 +0100

    implement first endpoint PS: dont change docstring

---
+++ web_flask/api/v1/app.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+""" 
+starts Flask app
+"""
+from flask import Flask
+
+app = Flask(__name__)
+app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
+
+@app.route('/status', strict_slashes=False)
+def status():
+    """status
+
+    Returns:
+        json: returns the status of our app
+    """
+    return {'status': 'OK'}
+
+if __name__ == "__main__":
+    """Main function
+    """
+    app.run('0.0.0.0', 5000)

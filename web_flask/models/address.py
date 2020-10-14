cf24b3795a51d192d835d6697c40af91156652d2
KoeusIss <sebri.issam@gmail.com>
2020-10-14 09:52:57 +0100

    just styling models

--- web_flask/models/address.py
+++ web_flask/models/address.py
@@ -2,12 +2,45 @@
 
 from web_flask.models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
 
 
 class Address(BaseModel, Base):
+    """
+        first_line: (str) address first line
+        second_line: (str) address second line
+        city: (str) the city
+        zip_code: (list) zip_code
+        state_id: (str) a reference to address's state
+
+    """
     __tablename__ = "addresses"
-    state_id = Column(String(60), ForeignKey("states.id"), nullable=False)
-    first_line = Column(String(128), nullable=False)
-    second_line = Column(String(128), default=None)
-    city = Column(String(128), nullable=False)
-    zip_code = Column(String(60), nullable=False)
+    state_id = Column(
+        String(60),
+        ForeignKey("states.id"),
+        nullable=False
+    )
+    first_line = Column(
+        String(128),
+        nullable=False
+    )
+    second_line = Column(
+        String(128),
+        default=None
+    )
+    city = Column(
+        String(128),
+        nullable=False
+    )
+    zip_code = Column(
+        String(60),
+        nullable=False
+    )
+    student_id = Column(
+        String(60),
+        ForeignKey('student.id')
+    )
+    student = relationship(
+        'Student',
+        backref='student'
+    )
--- web_flask/models/student.py
+++ web_flask/models/student.py
@@ -1,17 +1,47 @@
-#!/usr/bin/python3
-"""[summary]
-"""
+""" Student model """
 from web_flask.models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 
 
 class Student(BaseModel, Base):
+    """
+        first_name: (str) student first name
+        last_name: (str) student last name
+        middle_name: (str) middle name
+        id_number: (list) identity number
+        passport_number: (str) passport number
+        marital_status: (str) marital status
+        address: (str) reference to student's address
+
+    """
     __tablename__ = "students"
-    first_name = Column(String(128), nullable=False)
-    last_name = Column(String(128), nullable=False)
-    middle_name = Column(String(128))
-    id_number = Column(String(128), nullable=False)
-    passport_number = Column(String(128), default=None)
-    marital_status = Column(String(60), default="")
-    addresses = relationship("Address", uselist=False, backref="student", cascade="all, delete")
+    first_name = Column(
+        String(128),
+        nullable=False
+    )
+    last_name = Column(
+        String(128),
+        nullable=False
+    )
+    middle_name = Column(
+        String(128)
+    )
+    id_number = Column(
+        String(128),
+        nullable=False
+    )
+    passport_number = Column(
+        String(128),
+        default=None
+    )
+    marital_status = Column(
+        String(60),
+        default=""
+    )
+    address = relationship(
+        "Address",
+        uselist=False,
+        backref="student",
+        cascade="all, delete"
+    )

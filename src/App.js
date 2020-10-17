96bfb48450043d808053819bb0d360b2b3f012e8
KoeusIss <sebri.issam@gmail.com>
2020-10-17 16:44:01 +0100

    commit the first try with frontend

--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import Home from './views/home'
+import Students from "./views/students";
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <h2>Hello @ Holbietheque</h2>
-    </div>
+    <Router>
+        <ul>
+            <li>
+                <Link to='/'>Home</Link>
+            </li>
+            <li>
+                <Link to='/students'>Students</Link>
+            </li>
+        </ul>
+        <Switch>
+            <Route path='/' exact={true} >
+                <Home/>
+            </Route>
+            <Route path='/students' >
+                <Students/>
+            </Route>
+        </Switch>
+    </Router>
   );
 }
 

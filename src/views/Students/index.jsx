a9658779485abc63746c6aae0ecda5373115863a
IhebChatti <iheb.chatti@holbertonschool.com>
2020-10-23 16:14:44 +0100

    students list design

--- src/views/Students/index.jsx
+++ src/views/Students/index.jsx
@@ -11,6 +11,8 @@ import {
   Segment,
   Icon,
   Pagination,
+  Container,
+  Divider,
 } from "semantic-ui-react";
 import SearchExampleStandard from "../../components/search";
 
@@ -31,52 +33,56 @@ const Students = () => {
       <Grid columns={2}>
         <Grid.Row>
           <Grid.Column>
-            <Header as="h2">
+            <Header as="h2" style={{ margin: "auto 10% auto 5%" }}>
               <Image src={require("../../images/students_list.png")} />
               Students list
             </Header>
           </Grid.Column>
-          <Grid.Column floated="right">
+          {/* <Grid.Column width="1">
             <SearchExampleStandard source={students} />
-          </Grid.Column>
+          </Grid.Column> */}
         </Grid.Row>
-        <Grid.Row>
-          <Card.Group>
-            {students.map((student) => {
-              return (
-                <div>
-                  <Card>
-                    <Card.Content>
-                      <Image
-                        floated="right"
-                        size="tiny"
-                        rounded="true"
-                        wrapped
-                        src={student.image}
-                      />
-                      <Card.Header>{student.full_name}</Card.Header>
-                      <Card.Meta>Cohort #{student.cohort.name}</Card.Meta>
-                      <Card.Meta>{student.specialization.name}</Card.Meta>
-                      <Card.Description>{student.about_me}</Card.Description>
-                    </Card.Content>
-                    <Card.Content extra>
-                      <div className="ui two buttons">
-                        <Button
-                          basic
-                          color="blue"
-                          as={NavLink}
-                          to={"/students/" + student.id}
-                        >
-                          View
-                        </Button>
-                      </div>
-                    </Card.Content>
-                  </Card>
-                </div>
-              );
-            })}
-          </Card.Group>
+        <Grid.Row style={{ margin: "auto 10% auto 15%" }}>
+          <Container>
+            <Card.Group doubling>
+              {students.map((student) => {
+                return (
+                  <div>
+                    <Card centered>
+                      <Card.Content>
+                        <Image
+                          floated="right"
+                          verticalAlign="top"
+                          size="tiny"
+                          rounded="true"
+                          src={student.image}
+                        />
+                        <Card.Header>{student.full_name}</Card.Header>
+                        <Card.Meta>Cohort #{student.cohort.name}</Card.Meta>
+                        <Card.Meta>{student.specialization.name}</Card.Meta>
+                        <Card.Description>{student.about_me}</Card.Description>
+                      </Card.Content>
+                      <Card.Content extra>
+                        <div className="ui two buttons">
+                          <Button
+                            basic
+                            color="blue"
+                            as={NavLink}
+                            to={"/students/" + student.id}
+                          >
+                            View
+                          </Button>
+                        </div>
+                      </Card.Content>
+                    </Card>
+                    <Divider inverted />
+                  </div>
+                );
+              })}
+            </Card.Group>
+          </Container>
         </Grid.Row>
+
         {/* <Pagination
           defaultActivePage={5}
           ellipsisItem={{
